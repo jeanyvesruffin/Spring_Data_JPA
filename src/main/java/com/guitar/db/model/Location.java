0580baade770d06449e56f83a5d3cf4981df132a
jeanyvesruffin <ruffinmax@hotmail.fr>
2020-05-11 16:57:17 +0200

    Bug fix

--- src/main/java/com/guitar/db/model/Location.java
+++ src/main/java/com/guitar/db/model/Location.java
@@ -14,7 +14,7 @@ import javax.persistence.OneToMany;
 @Entity
 public class Location {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	private String state;
--- src/main/java/com/guitar/db/model/Manufacturer.java
+++ src/main/java/com/guitar/db/model/Manufacturer.java
@@ -17,20 +17,20 @@ import javax.persistence.OneToMany;
 
 @Entity
 @NamedNativeQuery(name = "Manufacturer.getAllThatSellAcoustics", 
-		query = "SELECT m.id, m.name, m.foundedDate, m.averageYearlySales, m.location_id as headquarters_id "
-	    + "FROM Manufacturer m "
+query = "SELECT m.id, m.name, m.foundedDate, m.averageYearlySales, m.location_id as headquarters_id "
+		+ "FROM Manufacturer m "
 		+ "LEFT JOIN Model mod ON (m.id = mod.manufacturer_id) "
 		+ "LEFT JOIN ModelType mt ON (mt.id = mod.modeltype_id) "
-	    + "WHERE (mt.name = ?)", resultClass = Manufacturer.class)
+		+ "WHERE (mt.name = ?)", resultClass = Manufacturer.class)
 public class Manufacturer {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
 	private Date foundedDate;
 	private BigDecimal averageYearlySales;
-	
+
 	@OneToMany(cascade=CascadeType.ALL)
 	@JoinColumn(name="MANUFACTURER_ID")
 	private List<Model> models = new ArrayList<Model>();
